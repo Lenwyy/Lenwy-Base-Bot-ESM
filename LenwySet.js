e1504b7929595e6d980eb93f855542be1904b011
Lenwy <154850207+Lenwyy@users.noreply.github.com>
2026-01-04 23:54:09 +0700

    Add files via upload

---
+++ LenwySet.js
@@ -0,0 +1,73 @@
+/*  
+
+  Made By Lenwy
+  Base : Lenwy
+  WhatsApp : wa.me/6283829814737
+  Telegram : t.me/ilenwy
+  Youtube : @Lenwy
+
+  Channel : https://whatsapp.com/channel/0029VaGdzBSGZNCmoTgN2K0u
+
+  Copy Code?, Recode?, Rename?, Reupload?, Reseller? Taruh Credit Ya :D
+
+  Mohon Untuk Tidak Menghapus Watermark Di Dalam Kode Ini
+
+*/
+
+import chalk from 'chalk';
+import figlet from 'figlet';
+import { promisify } from 'util';
+
+const terminalWidth = process.stdout.columns;
+const maxWidth = Math.min(terminalWidth, 50);
+
+// Konfigurasi Bot
+const config = {
+  whatsapp: true,
+  telegram: false // Tahap Pengembangan, Belum Bisa Digunakan
+};
+
+// Fungsi utama
+(async () => {
+  try {
+    if (config.whatsapp) {
+      console.log(chalk.green.bold('\n🎁  Menjalankan Lenwy Bot WhatsApp'));
+      const { default: startWhatsApp } = await import('./WhatsApp/index.js');
+      startWhatsApp(); // Jika kamu export default fungsi
+    } else {
+      console.log(chalk.red.bold('\n❌  Bot WhatsApp Dinonaktifkan Di LenwySet.js'));
+    }
+
+    if (config.telegram) {
+      console.log(chalk.green.bold('\n🎁  Menjalankan Lenwy Bot Telegram'));
+      const { default: startTelegram } = await import('./Telegram/index.js');
+      startTelegram();
+    } else {
+      console.log(chalk.red.bold('\n❌  Bot Telegram Dinonaktifkan Di LenwySet.js\n'));
+    }
+
+    const asyncFiglet = promisify(figlet.text);
+    const logo = await asyncFiglet('Lenwy', {
+      font: 'ANSI Shadow',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: maxWidth,
+      whitespaceBreak: false
+    });
+
+    console.log(chalk.blue.bold(logo));
+
+    console.log(chalk.white.bold(`${chalk.green.bold("📃  Informasi :")}         
+✉️  Script Lenwy Rebuild
+✉️  Author : Lenwy
+✉️  Gmail : ilenwyy@gmail.com
+✉️  Instagram : Ilenwy_
+✉️  Youtube : Lenwy
+🎁  Base : Lenwy
+
+${chalk.green.bold("🎁  Subscribe Lenwy :D")}\n`));
+
+  } catch (err) {
+    console.error(chalk.red.bold('\n⚠️  Terjadi Kesalahan : ' + err.message + '\n'));
+  }
+})();
---
+++ WhatsApp/database/Menu/LenwyMenu.js
@@ -0,0 +1,24 @@
+// Menu =============================
+
+globalThis.lenwymenu = `☕ *Lenwy Rebuild*
+*WhatsApp Bot To Solve Your Problems*
+
+📑 *Information Bot*
+*Creator : Lenwy*
+*Contact : wa.me/6283829814737*
+*Youtube Channel : Lenwy*
+
+[+] .Aimenu - Ai Menu
+[+] .Downmenu - Downloader Menu
+
+⚠ *Dalam Tahap Pengembangan*`,
+
+// AI Menu =========================
+
+globalThis.aimenu = `☕ *Menampilkan AI Menu*
+[+] .Ai - Tanya Apa Saja`
+
+// Down Menu =========================
+
+globalThis.downmenu = `☕ *Menampilkan Downloader Menu*
+[+] .tt - Download TikTok`
